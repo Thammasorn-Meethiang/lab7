f76313cf6302884aace347565b1074b6cc5fe809
Pleum <thammasorn_m@cmu.ac.th>
2025-12-16 13:38:16 +0700

    lab 7 3

---
+++ lab7_3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+char before(char x){
+	if (x > 65 && x <= 90){
+		return --x;
+		}
+	else if (x == 65){
+		return 'Z';}
+
+	else{
+		return '0';}
+}
+
+int main(){
+	//Test Case
+	cout << before('A') << "\n";
+	cout << before('B') << "\n";
+	cout << before('P') << "\n";
+	cout << before('T') << "\n";
+	cout << before('Z') << "\n";
+	cout << before('a') << "\n";
+	cout << before('0') << "\n";
+	cout << before('c') << "\n";
+	return 0;
+}
